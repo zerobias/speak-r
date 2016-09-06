e48b9a0fa3a5aaf5e1a702cbc01e41e99fade53a
Dmitry Boldyrev <ribkatt@gmail.com>
2016-09-06 12:39:53 +0300

    Improve code reusing

--- app/util.js
+++ app/util.js
@@ -1,6 +1,5 @@
 const R = require('ramda')
 const debug = require('debug')
-const bucker = require('bucker')
 const addMessage = R.ifElse(R.complement(R.isNil),e=>R.pipe(R.of,R.prepend(e)),e=>R.identity)
 
 const tagvalue = (tag,mess)=>R.isNil(mess) ? tag : [tag,mess].join(':  ')
@@ -8,7 +7,7 @@ const log = tag=>mess=>debug(tagvalue(tag,mess))
 const pipelog = tag=>mess=>R.tap(log(tag)(mess))
 
 const consoleOpts = { console:{color:true} }
-const logmodule = tag=>bucker.createLogger(consoleOpts,tag)
+// const logmodule = tag=>bucker.createLogger(consoleOpts,tag)
 
 const arrayify = R.when(R.complement(R.is(Array)),R.of)
 
@@ -16,5 +15,5 @@ const toPipe = R.apply(R.pipe)
 
 const isString = R.is(String)
 module.exports = {
-  pipelog,log,logmodule,isString,arrayify,toPipe
+  pipelog,log,isString,arrayify,toPipe
 }
