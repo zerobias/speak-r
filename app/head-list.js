74e270fadff0d159e27911f3808a85e1c5497c72
Dmitry Boldyrev <ribkatt@gmail.com>
2016-09-10 04:24:42 +0300

    Head-list realisation of lexemes

---
+++ app/head-list.js
@@ -0,0 +1,36 @@
+const R = require('ramda')
+const S = require('sanctuary')
+
+class HeadList {
+  constructor(rawList, head) {
+    if (!R.is(Array,rawList)||R.isEmpty(rawList)) return S.Left('No array recieved')
+    if (R.isNil(head)) {
+      this.head = R.head(rawList)
+      this.tail = R.tail(rawList)
+    } else {
+      this.head = head
+      this.tail = rawList||[]
+    }
+  }
+  map(func) {
+    return R.map(func,this.tail)
+  }
+  append(e) {
+    this.tail.push(e)
+    return this
+  }
+  static hasTail(list) {return R.has('tail',list)&&!R.isEmpty(list.tail)}
+  static last(list) {
+    return HeadList.hasTail(list)
+      ? R.last(list.tail)
+      : list.head
+  }
+  static lastR(list) {
+    return R.until(R.complement(R.has('tail')),HeadList.last)(list)
+  }
+  static emptyList() {
+    return new HeadList([{}])
+  }
+}
+
+module.exports = HeadList
--- app/junk.js
+++ app/junk.js
@@ -1,10 +1,6 @@
 const R = require('ramda')
 const S = require('sanctuary')
 
-
-const mValueComp = (val,field)=>data=>S.Maybe.of(R.propEq(R.defaultTo('value',field),val)).ap(data).value
-const isOpenS = mValueComp('<-')
-const isCloseS = mValueComp(',')
 const balanceReducer = (isOpen,isClose)=>arr=> {
   if (!R.is(Array,arr)) return S.Left('No array recieved')
   let i = 0
@@ -23,33 +19,5 @@ const balanceReducer = (isOpen,isClose)=>arr=> {
   })
 }
 //log('balance')(balanceReducer(isOpenS,isCloseS)(justData).value)
-class HeadList {
-  constructor(rawList, head) {
-    if (!R.is(Array,rawList)||R.isEmpty(rawList)) return S.Left('No array recieved')
-    if (R.isNil(head)) {
-      this.head = R.head(rawList)
-      this.tail = R.tail(rawList)
-    } else {
-      this.head = head
-      this.tail = rawList||[]
-    }
-  }
-  map(func) {
-    return R.map(func,this.tail)
-  }
-  append(e) {
-    this.tail.push(e)
-    return this
-  }
-  static hasTail(list) {return R.isEmpty(list.tail)}
-  static last(list) {
-    return HeadList.hasTail(list)
-      ? R.last(list.tail)
-      : list.head
-  }
-  static emptyList() {
-    return new HeadList([{}])
-  }
-}
 
-module.exports = { HeadList, balanceReducer }
+module.exports = { balanceReducer }
